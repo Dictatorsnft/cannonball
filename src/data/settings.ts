7f4fe936cc9bced3a14ac0127967311f1c6f41b6
SoLotto Club <110166056+Dictatorsnft@users.noreply.github.com>
2024-03-29 05:30:33 +0500

    psuh

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,6 +1,6 @@
 export const settings = {
-  site: 'https://cannonball.littlesticks.dev',
-  name: 'Little Sticks',
-  title: 'Cannonball by Little Sticks | A splash page template',
-  description: 'This is a simple splash page template built with Astro by Little Sticks',
+  site: 'https://myfactoryprints.com',
+  name: 'Factory Prints',
+  title: 'Factory Prints | Your factory for actuve wears and lifestyle apparel.',
+  description: 'As a leading designer, manufacturer and marketer of high-performance active wear and spirit wear for teams, coaches, athletes, Factory Prints is all about being able to customize how you want it.',
 }
